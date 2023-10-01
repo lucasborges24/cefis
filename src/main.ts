bf941cbe08aed5ae769e820bad5c698d09a48321
Lucas Borges Barbosa <lucasborgesbarbosa2012@gmail.com>
2023-09-30 22:17:27 -0300

    refactor: enable cors from any origin in production

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,13 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  if (process.env.NEST_ENV === 'development') {
-    app.enableCors({
-      origin: true,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      credentials: true,
-    });
-  }
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
